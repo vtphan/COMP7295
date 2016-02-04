1fbc8247f22dc46ac1cc5d0820b58e97fcf8eac2
Vinhthuy Phan <vphan@memphis.edu>
2016-02-04 09:20:49 -0600

    .

--- codes/matrix.py
+++ codes/matrix.py
@@ -1,9 +1,14 @@
 '''
-- blosum: read headerless csv-formatted blosum files
+- blosum: read blosum files
 - example: blosum("blosum60.txt")
 '''
 
-def blosum(csv_file):
+import re
+
+def clean_line(line):
+	return [ i for i in re.split('\s', line.strip()) if i!='' ]
+
+def blosum_csv(csv_file):
 	lines = open(csv_file).readlines()
 	aa = lines.pop(0).strip().split(',')
 	mat = {}
@@ -15,3 +20,25 @@ def blosum(csv_file):
 	return mat
 
 
+def blosum(fn):
+	lines = open(fn).readlines()
+	lines = [ l for l in lines if not l.startswith('#')]
+	
+	if fn.endswith('.csv'):
+		aa = lines.pop(0).strip().split(',')
+	else:
+		aa = clean_line(lines.pop(0))
+
+	mat = {}
+	for line in lines:
+		if fn.endswith('.csv'):
+			items = line.strip().split(',')
+		else:
+			items = clean_line(line)
+		a = items.pop(0)
+		for i in range(len(items)):
+			mat[(a,aa[i])] = int(items[i])
+	return mat
+
+# print(blosum("../data/blosum62.csv"))
+print(blosum("pam150.txt"))
