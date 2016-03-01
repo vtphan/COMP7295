f21c5fc61a5ee2c7d63c6028a5250db3f2237ccf
vtphan <vphan@memphis.edu>
2016-03-01 09:51:49 -0600

    .

--- codes/upgma.py
+++ codes/upgma.py
@@ -1,3 +1,11 @@
+def Initialize(distance_file, distance, label):
+	with open(distance_file) as f:
+		for line in f:
+			items = line.strip().split(" ")
+			label.append(items[0])
+			for i, lab in enumerate(label):
+				if i<len(Label)-1:
+					distance[lab,items[0]] = distance[items[0],lab] = float(items[i+1])
 
 class Node(object):
 	def __init__(self, label, left=None, right=None):
@@ -16,15 +24,11 @@ class Node(object):
 			print(")", end="")
 
 
+A = Node('A'); B = Node('B'); C = Node('c', A, B); D = Node('D'); root = Node('e', D, C)
+root.Newick()
 
-def Initialize(distance_file, distance, label):
-	with open(distance_file) as f:
-		for line in f:
-			items = line.strip().split(" ")
-			label.append(items[0])
-			for i, lab in enumerate(label):
-				if i<len(Label)-1:
-					distance[lab,items[0]] = distance[items[0],lab] = float(items[i+1])
-
+Distance = {}
+Label = []
 Initialize("phylo_distance.txt", Distance, Label)
+print( Distance['A','B'], Distance['B','A'])
 
